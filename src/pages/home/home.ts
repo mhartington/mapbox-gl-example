d876142eba1d59fe55bbb72720c5c3fe7f350d87
mhartington <mikehartington@gmail.com>
2017-12-01 10:30:03 -0500

    add mapbox example

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  @ViewChild('map') map: ElementRef;
+  constructor(public navCtrl: NavController) {}
+  ionViewDidLoad() {
+    mapboxgl.accessToken ='provide your own';
+    new mapboxgl.Map({
+      container: this.map.nativeElement,
+      style: 'mapbox://styles/mapbox/dark-v9',
+      center: [-122.420679, 37.772537],
+      zoom: 13
+    });
   }
-
 }
